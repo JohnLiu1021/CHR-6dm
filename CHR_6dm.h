f231b93b8ffbe19a8a4e827b5288378fe1a57892
John Liu <tw00084811@gmail.com>
2013-10-08 18:18:40 +0800

    Adding CHR_PacketType.h for packet verifying and little about CHR_6dm class

---
+++ CHR_6dm.h
@@ -0,0 +1,20 @@
+#ifndef CHR_6DM_H
+#define CHR_6DM_H 1
+
+#include "SerialPort_cpp.h"
+#include "CHR_SensorData.h"
+
+class CHR-6dm : public SerialPort {
+public:
+	CHR-6dm();
+	CHR-6dm(const char *path);
+	~CHR-6dm();
+
+	int Open
+
+	
+
+
+
+
+#endif
